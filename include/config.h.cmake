8d61ffbcad2ab277c20666d01e141c6661542829
Ralph Juhnke <r_juhnke@gmx.de>
2010-03-09 23:40:01 +0100

    added cmake support

---
+++ include/config.h.cmake
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PACKAGE @PACKAGE@
+#define VERSION @VERSION@
+#define ENABLE_NLS @ENABLE_NLS@
+#if ENABLE_NLS
+#define LOCALEDIR @LOCALEDIR@
+#endif
+
+#endif
